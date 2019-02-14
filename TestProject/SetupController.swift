93aebea5520bdb36d1b140547e6d89c9398a8a92
Ljubisa Katana <>
2019-02-14 14:46:28 +0100

    Add Xcode project and gitignore

---
+++ TestProject/SetupController.swift
@@ -0,0 +1,20 @@
+//
+//  SetupController.swift
+//  TestProject
+//
+//  Created by Ljubisa Katana on 2/14/19.
+//  Copyright © 2019 Ljubisa Katana. All rights reserved.
+//
+
+import UIKit
+
+class SetupController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
